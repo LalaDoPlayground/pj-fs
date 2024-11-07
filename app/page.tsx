f2b9369288a1e36ca63cd20ffe7e660db0aec6b7
LalaDoPlayground <larissa.durrewald@gmail.com>
2024-11-06 23:31:41 -0300

    feat: adicionei a autentificação com o clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">hello world</h1>
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName />
     </div>
   );
 };
